0f76f047c86a5bb48409f15caa7e11e726bb181b
9uilherme <9uilhermealencar@gmail.com>
2021-10-06 17:17:36 +0100

    override findAll

--- src/main/java/guidev/factory/repositories/SubscriptionRepository.java
+++ src/main/java/guidev/factory/repositories/SubscriptionRepository.java
@@ -2,6 +2,7 @@ package guidev.factory.repositories;
 
 import guidev.factory.models.Subscription;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +10,9 @@ import java.util.List;
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
 
-    List<Subscription> findAllByEventName(String name);
+    @Override
+    @Query("select sub from Subscription sub " +
+            "join fetch sub.channelConfig")
+    List<Subscription> findAll();
 
 }
